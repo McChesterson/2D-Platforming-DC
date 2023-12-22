9340b1ef5bbadbe5c1e3a5bc8a3d6142e91611c9
McChesterson <o.a.onstott@gmail.com>
2023-12-21 19:47:09 -0800

    small level changes

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelExit : MonoBehaviour
 {
+    public Image fadeBlackImage;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
@@ -14,6 +21,15 @@ public class LevelExit : MonoBehaviour
         {
             nextSceneIndex = 0;
         }
+        StartCoroutine(FadeOut());
         SceneManager.LoadScene(nextSceneIndex);
+    }
+    IEnumerator FadeIn()
+    {
+        yield return new WaitForEndOfFrame();
+    }
+    IEnumerator FadeOut()
+    {
+        yield return new WaitForEndOfFrame();
     }
 }
